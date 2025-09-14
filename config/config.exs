fb98dec2bb4dfdbe27a2b3f3a8281ca01a33122d
Jake Bruun <jake.bruun@evntd.com>
2025-09-13 21:06:40 -0700

    feat: adds Fact.EventStreamCategoryIndexer
    This indexs events by the leading portion of its stream name before the first '-'. This can be override using :separator.
    
    This is similar to the by_category projection in EventStoreDB, although it is just an index, it does not create $ce-{category} streams containing link events.

--- config/config.exs
+++ config/config.exs
@@ -3,11 +3,27 @@ import Config
 config :fact,
   indexers: [
     [enabled: true, 
-      mod: Fact.EventStreamIndexer, opts: [path: ".fact/indices/event_stream"]],
+      mod: Fact.EventStreamIndexer, 
+      opts: [
+        path: ".fact/indices/event_stream"]],
+    
     [enabled: true, 
-      mod: Fact.EventTypeIndexer, opts: [path: ".fact/indices/event_type"]],
+      mod: Fact.EventTypeIndexer, 
+      opts: [
+        path: ".fact/indices/event_type"]],
+    
+    [enabled: true,
+      mod: Fact.EventStreamCategoryIndexer, 
+      opts: [
+        path: ".fact/indices/event_stream_category", 
+        opts: [
+          separator: "-"]]],
+  
     [enabled: false, 
-      mod: Fact.EventDataIndexer, opts: [path: ".fact/indices/event_data", encoding: {:hash, :sha}]]
+      mod: Fact.EventDataIndexer, 
+      opts: [
+        path: ".fact/indices/event_data", 
+        encoding: {:hash, :sha}]]
   ],
   paths: [
     events: ".fact/events"
---
+++ lib/fact/event_stream_category_indexer.ex
@@ -0,0 +1,15 @@
+defmodule Fact.EventStreamCategoryIndexer do
+  @moduledoc false
+  use Fact.EventIndexer
+  
+  @impl true
+  def index_event(event, opts) do
+    separator = Keyword.get(opts, :separator, "-")
+    case event[@event_stream] do
+      nil -> nil 
+      stream ->
+        String.split(stream, separator, parts: 2) 
+        |> List.first()
+    end
+  end
+end
