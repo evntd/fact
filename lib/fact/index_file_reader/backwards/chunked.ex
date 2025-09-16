f8e1e8546057571c1a4b111b7a07fa15607e5af0
Jake Bruun <jake.bruun@evntd.com>
2025-09-15 20:13:18 -0700

    feat: adds a placeholder for a chunked backwards reader impl

---
+++ lib/fact/index_file_reader/backwards/chunked.ex
@@ -0,0 +1,8 @@
+defmodule Fact.IndexFileReader.Backwards.Chunked do
+  @moduledoc false
+
+  def read(_path) do
+    # TODO Implement a chunked backwards reader
+    raise "not implemented"
+  end
+end
