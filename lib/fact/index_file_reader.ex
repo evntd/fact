a7f726175bf844fdab01202c77e83b8cbb70f4ef
Jake Bruun <jake.bruun@evntd.com>
2025-09-15 20:38:57 -0700

    refactor: replace trim with slice
    Since the length of each line is known (32 chars + newline), slice is faster as it will not scan the string. This seems like a micro optimization, but will save a lot of CPU cycles and reduce memory churn.

--- lib/fact/index_file_reader.ex
+++ lib/fact/index_file_reader.ex
@@ -1,8 +1,10 @@
 defmodule Fact.IndexFileReader do
   @moduledoc false
+  
+  @event_id_length 32
 
   def read_forward(path) do
-    File.stream!(path) |> Stream.map(&String.trim/1)
+    File.stream!(path) |> Stream.map(&String.slice(&1,0,@event_id_length))
   end
 
   defdelegate read_backward(path), to: Fact.IndexFileReader.Backwards.Line, as: :read
