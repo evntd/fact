690bd6cec0f4846c41b656d1395a1caa3ef324bb
Jake Bruun <jake.bruun@evntd.com>
2025-09-24 21:29:14 -0700

    refactor: rename Fact.EventReader.Json to Fact.EventFileReader.Json

--- lib/fact/event_file_reader/json.ex
+++ lib/fact/event_file_reader/json.ex
@@ -1,4 +1,4 @@
-defmodule Fact.EventReader.Json do
+defmodule Fact.EventFileReader.Json do
   @moduledoc false
   @file_extension ".json"
   def read(path) do
--- lib/fact/event_reader.ex
+++ lib/fact/event_reader.ex
@@ -5,7 +5,7 @@ defmodule Fact.EventReader do
     do_read_event(Path.join(db_dir(), event_id))
   end
 
-  defdelegate do_read_event(path), to: Fact.EventReader.Json, as: :read
+  defdelegate do_read_event(path), to: Fact.EventFileReader.Json, as: :read
 
   defp db_dir, do: Application.get_env(:fact, :db)
 end
