6357c91af278b5ce8ad5c2eef7f49134f66b4d95
Jake Bruun <jake.bruun@evntd.com>
2025-12-13 08:26:04 -0800

    feat: adds flag to check format
    add elixir 1.13 and 1.14 back to the test workflow.

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Fact.MixProject do
     [
       app: :fact,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
