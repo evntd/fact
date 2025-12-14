d2a97112ea1f28fbec9aa1e12219d017a825d8db
Jake Bruun <jake.bruun@evntd.com>
2025-12-13 22:23:24 -0800

    chore: remove unused Fact.Names.subscription/1

--- lib/fact/names.ex
+++ lib/fact/names.ex
@@ -2,8 +2,7 @@ defmodule Fact.Names do
   @moduledoc false
 
   def registry(instance), do: :"#{instance}.Fact.Registry"
-  def subscription(instance), do: :"#{instance}.Fact.EventPublisher"
-
+  
   def via(instance, key) do
     {:via, Registry, {registry(instance), key}}
   end
