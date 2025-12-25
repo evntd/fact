c66ac267e12cc94258e8b3410961f7e3c3697196
Jake Bruun <jake.bruun@evntd.com>
2025-12-25 12:12:32 -0800

    feat: adds init func to Fact.Context to get all the defaults.

--- lib/fact/context.ex
+++ lib/fact/context.ex
@@ -25,4 +25,27 @@ defmodule Fact.Context do
     :record_schema,
     :storage_layout
   ]
+
+  def init() do
+    %__MODULE__{
+      index_checkpoint_file_content: Fact.IndexCheckpointFileContent.init(),
+      index_checkpoint_file_name: Fact.IndexCheckpointFileName.init(),
+      index_checkpoint_file_reader: Fact.IndexCheckpointFileReader.init(),
+      index_checkpoint_file_writer: Fact.IndexCheckpointFileWriter.init(),
+      index_file_content: Fact.IndexFileContent.init(),
+      index_file_name: Fact.IndexFileName.init(),
+      index_file_reader: Fact.IndexFileReader.init(),
+      index_file_writer: Fact.IndexFileWriter.init(),
+      ledger_file_content: Fact.LedgerFileContent.init(),
+      ledger_file_name: Fact.LedgerFileName.init(),
+      ledger_file_reader: Fact.LedgerFileReader.init(),
+      ledger_file_writer: Fact.LedgerFileWriter.init(),
+      record_file_content: Fact.RecordFileContent.init(),
+      record_file_name: Fact.RecordFileName.init(),
+      record_file_reader: Fact.RecordFileReader.init(),
+      record_schema: Fact.RecordSchema.init(),
+      record_file_writer: Fact.RecordFileWriter.init(),
+      storage_layout: Fact.StorageLayout.init()
+    }
+  end
 end
