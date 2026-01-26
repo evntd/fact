0af631aa1d9440ce3edc98cb983a0384a0a9f80a
Jake Bruun <jake.bruun@evntd.com>
2026-01-25 17:34:06 -0800

    refactor: adds helpers to retrieve event data fields by atom or string.

--- lib/fact/event_data_indexer.ex
+++ lib/fact/event_data_indexer.ex
@@ -1,6 +1,6 @@
 defmodule Fact.EventDataIndexer do
   @moduledoc """
-  Index events by the values of a specified key within the event data.
+  Indexes events by the values of a specified field within event data.
   """
   use Fact.EventIndexer
 
@@ -25,44 +25,24 @@ defmodule Fact.EventDataIndexer do
   ### Options
 
     * `:indexer_key` — required, specified the field to lookup within the event data
-
-  ### Examples
-
-      iex> event = %{
-      ...>   "event_type" => "ClutchLaid", 
-      ...>   "event_data" => %{"turtle_id" => "t1", "clutch_id" => "c1", "eggs" => 42}, 
-      ...>   "event_tags" => ["turtle:t1", "clutch:c1"], 
-      ...>   "stream_id" => "turtle_mating-1234",
-      ...>   "stream_position" => 3
-      ...> }
-      iex> Fact.EventDataIndexer.index_event(event, [indexer_key: "eggs"])
-      "42"
-
-      iex> event = %{
-      ...>   "event_type" => "EggHatched", 
-      ...>   "event_data" => %{"turtle_id" => "t2", "clutch_id" => "c1"}, 
-      ...>   "event_tags" => ["turtle:t2", "clutch:c1"], 
-      ...> } 
-      iex> Fact.EventDataIndexer.index_event(event, [indexer_key: "turtle_id"])
-      "t2"
-
-      iex> event = %{
-      ...>   "event_type" => "DatabaseCreated", 
-      ...>   "event_data" => %{"database_id" => "RVX27QR6PFDORJZF24C4DIICSQ"}, 
-      ...>   "stream_id" => "__fact", 
-      ...>   "stream_position" => "1"
-      ...> }
-      iex> Fact.EventDataIndexer.index_event(event, [indexer_key: "turtle_id"])
-      nil
-
   """
   @impl true
   def index_event(schema, event, opts) do
     event_data = event[schema.event_data]
     indexer_key = Keyword.get(opts, :indexer_key)
 
-    unless is_nil(value = Map.get(event_data, indexer_key)) do
+    unless is_nil(value = get(event_data, indexer_key)) do
       to_string(value)
     end
+  end
+
+  defp get(map, key) when is_binary(key) do
+    case Map.fetch(map, key) do
+      {:ok, value} -> value
+      :error -> Map.get(map, String.to_existing_atom(key))
+    end
+  rescue
+    ArgumentError ->
+      nil
   end
 end
